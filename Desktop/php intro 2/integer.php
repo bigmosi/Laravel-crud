2653635a24c6dd61336f5a1dd0914eaf40cbabe4
kinyeramos2021 <kinyeramo@gmail.com>
2021-03-05 21:43:31 +0300

    integer.php

--- Desktop/php intro 2/integer.php
+++ Desktop/php intro 2/integer.php
@@ -1,13 +1,8 @@
 <?php
 
-$authors = array("Charles Dickens","Jane Austin","William Shakespear","Mark Twain","Louse May Alcott");
-
-$books = ["Little Woman","Jane Eyre", "Richard III","Oliver Twist"];
-
-$allTypes = [10,1.3,"howdy",true];
-
-print_r($allTypes);
-
-print_r($books);
-
+$authors = array(
+    "quarkly" => "Charles Dickens",
+    10=>"Jane Austin",
+    "peotic"=>"William Shakespear",
+    );
 print_r($authors);
