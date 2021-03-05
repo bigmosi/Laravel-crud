dd3e46fb6dcdbf53062eb2330b0fac187a9f51a7
kinyeramos2021 <kinyeramo@gmail.com>
2021-03-05 19:48:45 +0300

    integer.php

--- Desktop/php intro 2/integer.php
+++ Desktop/php intro 2/integer.php
@@ -2,15 +2,21 @@
 class Fruit{
 public $name;
 public $color;
-function __construct($name){
+function __construct($name, $color){
     $this->name = $name;
+    $this->color = $color;
 }
 function get_name(){
     return $this->name;
+    
+}
+function get_color(){
+    return $this->color;
 }
 
 }
 
-$apple = new Fruit("Apple\n");
+$apple = new Fruit("Apple\n", "Red\n");
 
 echo $apple->get_name();
+echo $apple->get_color();
