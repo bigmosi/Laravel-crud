ad862a86d1a0caf5487bffc3047d975961c82953
kinyeramos2021 <kinyeramo@gmail.com>
2021-03-14 22:02:49 +0300

    I have set my development server using devsever

--- Desktop/react/cra-test/webpack.config.dev.js
+++ Desktop/react/cra-test/webpack.config.dev.js
@@ -1,3 +1,18 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+process.env.Node_ENV = 'developement';
+
+module.exports = {
+    mode: 'development',
+    target: 'web',
+    devTool: 'cheap-module-source-map',
+    entry: './src/index',
+    output: {
+        path:path.resolve(_dirname,"build"),
+        publicPath: '/',
+        filename: 'bundle.js'
+    }
+}
