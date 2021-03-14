69f1506e3e587483d58f991020242e0ffc753282
kinyeramos2021 <kinyeramo@gmail.com>
2021-03-14 22:39:43 +0300

    I have configured my webpack

--- Desktop/react/cra-test/webpack.config.dev.js
+++ Desktop/react/cra-test/webpack.config.dev.js
@@ -14,5 +14,32 @@ module.exports = {
         path:path.resolve(_dirname,"build"),
         publicPath: '/',
         filename: 'bundle.js'
+    },
+    devserver:{
+        stats: 'minimal',
+        overlay: 'true',
+        historyApiFallback: 'true',
+        disableHostCheck: 'true',
+        headers: {"Access-Control-Allow-Origin": "*"};
+        https: false
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            favicon: "src/favicon.ico"
+        })
+    ],
+    module: {
+        rules : [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /(\.css)$/,
+                use: ["style-loader","css-loader"]
+            }
+        ]
     }
-}
+};
