d3b72be7ee83419a4d91cc5b9da568457bd2638e
kinyeramos2021 <kinyeramo@gmail.com>
2021-03-15 22:33:57 +0300

    Am adding style to my webpages

--- Desktop/onen/Bookshop/resources/views/welcome.blade.php
+++ Desktop/onen/Bookshop/resources/views/welcome.blade.php
@@ -3,16 +3,29 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <h1>WELCOME TO KOMPIRE WASHING BAY</h1>
-            @if(true)
-                <p>This only display if it is true</p>
-            @else
-            <p>This only display if it is false</p>
-                @endif
-            <hr>
-            @for($i=0; $i<5 ;$i++)
-                <p>{{$i+1}}.Iteration</p>
-            @endfor
+            <p class="quote">WELCOME TO KOMPIR WASHING BAY</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="post-title">Kompir Car Jet Washing Bay</h1>
+            <p>This blog will get you right on track with Laravel</p>
+            <p><a href="#">Read more...</a> </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="post-title">The next Steps</h1>
+            <p>Understanding the basics is great, but you need to be able to make the next step</p>
+            <p><a href="#">Read more...</a> </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="post-title">Laravel 5.3</h1>
+            <p>Though announced as a "minor release", Laravel Laravel 5.3 ships with somer very
+            interesting additions and features.</p>
+            <p><a href="#">Read more...</a> </p>
         </div>
     </div>
     @endsection
