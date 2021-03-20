26770e6212ed64f7ef27c628c508b8e2df2343cb
kinyeramos2021 <kinyeramo@gmail.com>
2021-03-20 16:28:15 +0300

    set function

--- freeCodeGram/resources/views/about.blade.php
+++ freeCodeGram/resources/views/about.blade.php
@@ -3,10 +3,16 @@
     @elseif(5==10)
        <p>5 is indeed lower than 10</p>
     @else
-    <P>All condition are wrong!</P>
+   <h1>All condition are wrong!</h1>
     @endif
 @unless(empty($name))
 
     <h2>Name isn't empty</h2>
-
     @endunless
+@empty(!$name)
+    <h2>Name is empty!</h2>
+@endempty
+
+@isset($name)
+    <h2>Name has been set</h2>
+    @endisset
